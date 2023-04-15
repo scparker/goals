1c5a99c28ce89e2972120a1238a378fa96c8d3d7
Steve Parker <steven@stevenparker.com>
2023-04-14 22:09:30 -0400

    First plot

--- goals.py
+++ goals.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import streamlit as st
 
 st.title("Goals!!!!!")
@@ -5,3 +7,8 @@ st.title("Goals!!!!!")
 st.write("Set Goals")
 
 st.write("Record Goals")
+
+
+lifts = [200, 170, 210, 160, 150]
+
+st.pyplot(lifts)
