f4d51b7901f2f47d6c93844011ab0e00c85aa0ea
Steve Parker <steven@stevenparker.com>
2023-04-14 22:17:31 -0400

    Added swole function.

--- goals.py
+++ goals.py
@@ -8,11 +8,15 @@ st.write("Set Goals")
 
 st.write("Record Goals")
 
-st.checkbox('Are you feeling swole today?')
+lifts = np.array([200, 170, 210, 160, 150])
 
-lifts = [200, 170, 210, 160, 150]
+if st.checkbox('Are you feeling swole today?'):
+    lifts = lifts * 2
 
 fig, ax = plt.subplots()
 ax.plot(lifts)
+ax.set_title("Lift History")
+ax.set_xlabel("Day")
+ax.set_ylabel("Pounds")
 
 st.pyplot(fig)
