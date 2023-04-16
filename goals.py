92f4ad119676b92b43a5b946ce117431518d1ac7
Steven C. Parker <steven@stevenparker.com>
2023-04-15 22:01:49 -0400

    Update goals.py

--- goals.py
+++ goals.py
@@ -11,6 +11,16 @@ def init_connection():
 
 client = init_connection()
 
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def get_data():
+    db = client.mydb
+    items = db.mycollection.find()
+    items = list(items)  # make hashable for st.cache_data
+    return items
+
+items = get_data()
+
 st.title("Goals!!!!!")
 
 st.write("Set Goals")
